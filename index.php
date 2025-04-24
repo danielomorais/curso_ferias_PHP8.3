b6bba62d80a7a7dfb6d3304cae80f1971bd928e6
Daniel Morais <dan.oliveira972@gmail.com>
2025-04-24 00:37:58 -0300

    Incluindo Bootstrap CSS

--- index.php
+++ index.php
@@ -22,20 +22,50 @@ try {
     $pdo = new PDO($dsn, $user, $password, $options);
     $id = $_GET['id'] ?? '';
     $sql = 'SELECT `p`.`id`, `c`.`titulo` AS `categoria`, `p`.`nome`, `p`.`descricao`, `p`.`preco` FROM produtos AS `p`
-            INNER JOIN categorias AS `c` ON `c`.`id` = `p`.`categoria_id`
-            WHERE `p`.`id` = :id
-            ';
-    $statement = $pdo->prepare($sql);
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->execute();
-    $produto = $statement->fetch();
-
-    if ($produto) {
-        echo "O produto {$produto['nome']}, da categoria {$produto['categoria']}, custa R$ " . number_format($produto['preco'], 2, ',', '.') . "<br>";
-    } else {
-        echo "ID do produto não fornecido ou inválido!";
-    }
-    
+            INNER JOIN categorias AS `c` ON `c`.`id` = `p`.`categoria_id` ';
+    $statement = $pdo->query($sql);
+    $produtos = $statement->fetchAll();
 } catch (PDOException $exception) {
     die("Não foi possível se conectar com o banco de dados. Motivo: {$exception->getMessage()}");
-};
+}; 
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha primeira aplicação com PHP + PDO + MySQL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="mt-5 mb-4">Meus produtos</h1>
+                
+                <table class="table table-dark table-striped">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome do produto</th>
+                        <th>Categoria do produto</th>
+                        <th>Preço do produto</th>
+                    </tr>
+
+                    <?php foreach($produtos as $produto): ?>
+                    <tr>
+                        <td><?php echo $produto['id']?></td>
+                        <td><?php echo $produto['nome']?></td>
+                        <td><?php echo $produto['categoria']?></td>
+                        <td><?php echo number_format($produto['preco'], 2, ',', '.'); ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
